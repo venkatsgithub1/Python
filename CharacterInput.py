6506253f772beff05f4cb40a19ba39d93277cd9a
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-01 23:50:47 +0530

    Create CharacterInput.py

---
+++ CharacterInput.py
@@ -0,0 +1,11 @@
+'''
+Exercise 1
+Create a program that asks the user to enter their name and their age. 
+Print out a message addressed to them that tells them the year that they will turn 100 years old.
+'''
+import datetime
+
+name=input("Enter the name")
+age=int(input ("Enter the age"))
+
+print (name+", You will turn 100 years in "+ str(datetime.datetime.now().year-age+100))
