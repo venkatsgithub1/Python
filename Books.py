35eb0b34367397f5186adf1b9ba2f7cb19453718
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-05 10:29:48 +0530

    Create Books.py
    The code uses a list to store data of book names and it's authors

---
+++ Books.py
@@ -0,0 +1,3 @@
+books=[[['Name','Mindset'],['Author','Carol S. Dweck']],[['Name','grit the power of passion and perseverance'],['Author','Angela Duckworth']]]
+
+print ("Book\t"+books[0][0][1]+"\nAuthor\t"+ books[0][1][1])
