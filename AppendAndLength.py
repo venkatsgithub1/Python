a46fa78e8ceb61ab083296606bd59a2921f08d5a
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-05 12:48:10 +0530

    Create AppendAndLength.py
    An example to describe usage of adding a list using append procedure at the end of another list.

---
+++ AppendAndLength.py
@@ -0,0 +1,12 @@
+p=[1,2]
+
+q=[3,4]
+
+#appending q to p using append procedure.
+p.append(q)
+
+#printing below statement produces two elements and a third element as a list.
+print (p)
+
+#the below statement produces the length of list p.
+print (len(p))
