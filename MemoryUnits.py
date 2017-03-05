62a6a8615331626b7b65ddb7d6736e7cbb21e9ef
venkatsgithub1 <venkatsgithub@gmail.com>
2017-03-05 16:27:23 +0530

    Create MemoryUnits.py
    Program that prints basic units that are used to represent memory.

---
+++ MemoryUnits.py
@@ -0,0 +1,5 @@
+#** is used to define exponents in Python.
+print ("1 Kilo Byte = ",2**10, "\t\tBytes")
+print ("1 Mega Byte = ", (2**10)**2, "\t\tBytes")
+print ("1 Giga Byte = ", (2**10)**3, "\tBytes")
+print ("1 Tera Byte = ", (2**10)**4, "\tBytes")
