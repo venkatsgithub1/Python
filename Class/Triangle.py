b9e32070486ea7ad44ae110b353ee530d968b6e4
venkatsgithub1 <venkatsgithub@gmail.com>
2017-06-03 00:09:24 +0530

    Create Triangle.py

---
+++ Class/Triangle.py
@@ -0,0 +1,7 @@
+class Triangle(object):
+    def __init__(self,angle1,angle2,angle3):
+        self.angle1=angle1
+        self.angle2=angle2
+        self.angle3=angle3
+        
+triangle=Triangle (1,2,3)
