025610909a169b48c299137e1202f5e58b77bc5b
venkatsgithub1 <venkatsgithub@gmail.com>
2017-06-03 13:37:28 +0530

    Create Inheritance1.py

---
+++ Codecademy/Classes/Inheritance1.py
@@ -0,0 +1,21 @@
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+        
+    def display_car(self):
+        return "This is a "+self.color+" "+self.model+" with "+str(self.mpg)+" MPG."
+        
+    def drive_car (self):
+        self.condition="used"
+        
+class ElectricCar (Car):
+    def __init__(self,model,color,mpg,battery_type):
+        self.model=model
+        self.color=color
+        self.mpg=mpg
+        self.battery_type=battery_type
+
+my_car = ElectricCar("DeLorean", "silver", 88,"molten salt")
