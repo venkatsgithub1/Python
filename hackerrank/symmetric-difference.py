753ed40ac029e21da29d7468d071f3adfc5891eb
venkatsgithub1 <venkatsgithub@gmail.com>
2017-07-20 20:07:57 +0530

    Delete symmetric-difference.py
    Moved to sets

--- hackerrank/symmetric-difference.py
+++
@@ -1,4 +0,0 @@
-m,mdata,n,ndata=int(input()),set(map(int,input().split())),int(input()),set(map(int,input().split()))
-list1=sorted([x for x in list(mdata.union(ndata).difference(mdata.intersection(ndata)))])
-for data in list1:
-    print(data)
